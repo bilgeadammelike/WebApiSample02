3b45b19ceca76b105d4978944ac4a055823e80f3
bilgeadammelike <bilgeadammelike@mynet.com>
2019-05-10 12:45:00 +0300

    Hoca Son Ders

---
+++ LessonTwo/Controllers/DenemeController.cs
@@ -0,0 +1,36 @@
+﻿using LessonTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LessonTwo.Controllers
+{
+    [RoutePrefix("api/Shipper")]
+    public class DenemeController : ApiController
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        //Shipper için Get Put Post Delete
+        //Controller a api/Shipper diyerek ulaşmalıyız.
+        //Get actionı GetShipperName olarak çağırıldığında cevap vermeli. 
+        //optinal = Get fonksiyonu Oqueryable çalışabilmeli.
+
+// localhost:45632/api/Shipper/GetShipperName
+
+        [Route(Name = "GetShipperName")]
+        public List<Shippers> Get()
+        {
+            return null;
+        }
+
+        public IHttpActionResult Post([FromBody]Shippers s)
+        {
+
+            return Ok();
+        }
+
+    }
+}
